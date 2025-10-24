5adbab598d78f5fe146ac05356379ea2c16394ae
PRAVEEN V <166225954+praveenveeramani@users.noreply.github.com>
2025-10-24 23:48:57 +0530

    Update API endpoint and model in chat function

--- groq_chat.py
+++ groq_chat.py
@@ -11,13 +11,13 @@ def chat_with_groq(prompt):
     you are helpfull assistant to response with a given user prompt
     """
     try:
-        url = "https://api.openai.com/openai/v1/chat/completions"
+        url = "https://api.groq.com/openai/v1/chat/completions"
         headers = {
             "Authorization": f"Bearer {GROQ_API_KEY}",
             "Content-Type": "application/json",
         }
         payload = {
-            "model": "openai/gpt-oss-20b",
+            "model": "gpt-4o-mini",
             "messages": [{"role": "user", "content": prompt}],
             "temperature": 0.7
         }
@@ -29,6 +29,7 @@ def chat_with_groq(prompt):
 
     except Exception as e:
         return f"Error: {str(e)}"
+
 
 
 
